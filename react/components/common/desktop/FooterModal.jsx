149610b8dae3da391ceedd09c114e944edc33a60
Chetan Bhuwania <chetan@numerin.io>
2018-04-27 15:19:51 +0530

    fixes for home/promo page on develop branch

--- react/components/common/desktop/FooterModal.jsx
+++ react/components/common/desktop/FooterModal.jsx
@@ -1,33 +1,63 @@
 import React, { PureComponent } from 'react';
 
 class FooterModal extends PureComponent {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="modal fade in" id="popupModal" role="dialog" style={{
-				'display': 'block'
-			}} onClick={this.props.onClose}>
-				<div className="modal-dialog" role="document">
-					<div className="modal-content">
-						<div className="modal-header">
-							<button type="button" className="close close-modal" data-dismiss="modal" aria-label="Close" onClick={this.props.onCloseBtn}>
-								<span aria-hidden="true">
-									<svg viewport="0 0 20 20" width="20" height="20" version="1.1" xmlns="http://www.w3.org/2000/svg">
-										<line x1="1" y1="20" x2="20" y2="1" stroke="black" strokeWidth="2"/>
-										<line x1="1" y1="1" x2="20" y2="20" stroke="black" strokeWidth="2"/>
-									</svg>
-								</span>
-							</button>
-							<h3 className="modal-title">{this.props.children[0]}</h3>
-						</div>
-						{this.props.children[1]}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      // eslint-disable-next-line
+      <div
+        className="modal fade in"
+        id="popupModal"
+        role="dialog"
+        style={{
+          display: 'block',
+        }}
+        onClick={this.props.onClose}
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="close close-modal"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={this.props.onCloseBtn}
+              >
+                <span aria-hidden="true">
+                  <svg
+                    viewport="0 0 20 20"
+                    width="20"
+                    height="20"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <line
+                      x1="1"
+                      y1="20"
+                      x2="20"
+                      y2="1"
+                      stroke="black"
+                      strokeWidth="2"
+                    />
+                    <line
+                      x1="1"
+                      y1="1"
+                      x2="20"
+                      y2="20"
+                      stroke="black"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <h3 className="modal-title">{this.props.children[0]}</h3>
+            </div>
+            {this.props.children[1]}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export { FooterModal };
--- react/components/common/desktop/MainpageSquare.jsx
+++ react/components/common/desktop/MainpageSquare.jsx
@@ -1,29 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 class SquareTop extends React.Component {
-	constructor (props) {
-		super(props);
-	}
-	render () {
-		return (
-			<ul className="sec1-list">
-				{this.props.children}
-			</ul>
-		)
-	}
+  render() {
+    return <ul className="sec1-list">{this.props.children}</ul>;
+  }
 }
 
 class SquareBot extends React.Component {
-	constructor (props) {
-		super(props);
-	}
-	render () { 
-		return (
-			<ul className="sec2-list">
-				{this.props.children}
-			</ul>
-		)
-	}
+  render() {
+    return <ul className="sec2-list">{this.props.children}</ul>;
+  }
 }
 
-export { SquareBot, SquareTop }
+export { SquareBot, SquareTop };
