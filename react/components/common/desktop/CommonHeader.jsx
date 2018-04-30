00f78761259ef5ff9f20b536350adc3283a20c02
Alex Starlight <alex@starlightgroup.io>
2018-04-30 15:32:35 +0300

    mobile home page

--- react/components/common/desktop/CommonHeader.jsx
+++ react/components/common/desktop/CommonHeader.jsx
@@ -1,15 +1,10 @@
 import React, { PureComponent } from 'react';
 
-const Logo = () => (
-  <a href="index.html">
-    <img className="logo" src="../static/assets/images/logo.png" alt="Logo" />
-  </a>
-);
 const Menu = props => (
-  <ul className="menu-list for-desk">
+  <React.Fragment>
     <li>
       <a href="home" className={props.currentPage === 'home' ? 'active' : ''}>
-        HOME
+        Home
       </a>
     </li>
     <li>
@@ -17,7 +12,7 @@ const Menu = props => (
         href="products"
         className={props.currentPage === 'products' ? 'active' : ''}
       >
-        PRODUCTS
+        Products
       </a>
     </li>
     <li>
@@ -27,7 +22,7 @@ const Menu = props => (
     </li>
     <li>
       <a href="cart" className={props.currentPage === 'cart' ? 'active' : ''}>
-        CART
+        Cart
       </a>
     </li>
     <li>
@@ -35,27 +30,77 @@ const Menu = props => (
         href="contact"
         className={props.currentPage === 'contact' ? 'active' : ''}
       >
-        CONTACT
+        Contact
       </a>
     </li>
-  </ul>
+  </React.Fragment>
 );
 
 class CommonHeader extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.state.currentPage = props.currentPage;
+    this.toggleClass = this.toggleClass.bind(this);
+    this.state = {
+      activeMobileMenu: false,
+      currentPage: props.currentPage,
+    };
+  }
+
+  toggleClass() {
+    this.setState({ activeMobileMenu: !this.state.activeMobileMenu });
   }
 
   render() {
     return (
-      <div className="top-sec">
-        <div className="container">
-          <Logo />
-          <Menu home="HOME" currentPage={this.state.currentPage} />
+      <React.Fragment>
+        <div
+          className={
+            this.state.activeMobileMenu ? 'overally ovrActv' : 'overally'
+          }
+          onClick={this.toggleClass}
+        />
+        <div className="top-sec">
+          <div className="container">
+            <a href="index.html">
+              <img
+                className="logo"
+                src="../static/assets/images/logo.png"
+                alt="Logo"
+              />
+            </a>
+            <div className="light-menu for-mob for-tab">
+              <div className="navwrp">
+                <a
+                  href="javascript:void(0)"
+                  className="smobitrigger mob-mnu inrwrpr"
+                  onClick={this.toggleClass}
+                >
+                  <img src="../static/assets/images/mob-menu-btn.png" alt="" />
+                </a>
+                <ul
+                  className={
+                    this.state.activeMobileMenu
+                      ? 'mobimenu inrwrp mnuopn'
+                      : 'mobimenu inrwrp'
+                  }
+                >
+                  <a
+                    href="javascript:void(0)"
+                    className="mnuclose"
+                    onClick={this.toggleClass}
+                  >
+                    X
+                  </a>
+                  <Menu />
+                </ul>
+              </div>
+            </div>
+            <ul className="menu-list for-desk">
+              <Menu home="HOME" currentPage={this.state.currentPage} />
+            </ul>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
