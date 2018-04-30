913cf4bbce29117aa3e38ad5d9a05c697de955be
Chetan Bhuwania <chetan@numerin.io>
2018-04-30 14:51:46 +0530

    added .scss support back

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const webpack = require('webpack');
 const Uglify = require('uglifyjs-webpack-plugin');
+const withSass = require('@zeit/next-sass');
 
-module.exports = {
+module.exports = withSass({
   webpack: (config, { dev }) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin');
@@ -11,4 +12,4 @@ module.exports = {
     }
     return config;
   },
-};
+});
