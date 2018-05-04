57cf02655e33b02d442ddefa47c71e88eff1c876
Chetan Bhuwania <chetan@numerin.io>
2018-05-05 00:27:05 +0530

    adds validator.js for complex validations, ex. email

--- helpers/formValidators.js
+++ helpers/formValidators.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 const shippingFormValidator = values => {
   const errors = {};
   if (!values.firstName) {
@@ -40,7 +42,7 @@ const shippingFormValidator = values => {
     // The value is not a valid email address
   }
 
-  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  if (values.email && !validator.isEmail(values.email)) {
     errors.email = 'The value is not a valid email address';
   }
 
