af14149ba90911f3fb8c4d9c55f276db1757d726
Nishchit14 <dnishchit@gmail.com>
2018-05-06 13:10:54 +0530

    address lable changes

--- react/components/promo/desktop/PromoCheckoutPaymentForm.js
+++ react/components/promo/desktop/PromoCheckoutPaymentForm.js
@@ -148,7 +148,7 @@ class PromoCheckoutPaymentForm extends React.Component {
               <Field
                 component={AddressField}
                 name="address"
-                label="Address Line 1"
+                label="Address 1"
                 placeholder="Street and number, P.O. box, c/o."
                 required
                 changeField={this.props.change}
@@ -156,7 +156,7 @@ class PromoCheckoutPaymentForm extends React.Component {
               <Field
                 component={TextField}
                 name="address2"
-                label="Address Line 2"
+                label="Address 2"
                 placeholder="Apartment, suite, unit, building, floor, etc."
               />
               <Field
