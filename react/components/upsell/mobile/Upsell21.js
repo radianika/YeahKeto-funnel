285e5a321f3e365e80c57fa4d715d1f385327794
Alex Klimov <alex@starlightgroup.io>
2018-05-24 18:16:18 +0300

    Issue - 404 error, mobile, v.2 Fix path

--- react/components/upsell/mobile/Upsell21.js
+++ react/components/upsell/mobile/Upsell21.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { PromoSession, Footer } from 'react/components/common';
 import { getQueryString } from 'helpers';
-import { Carousel } from './Carousel';
-import { SatisfactionBox } from './SatisfactionBox';
-import { Shortage } from '../Shortage';
-import { UpsellFooter } from '../UpsellFooter';
 import Link from 'next/link';
 
 class Upsell21 extends React.PureComponent {
-  upgrade = productId => {
+  upgrade = () => {
     this.props.upgrade(215, '/promo/mobile/thankyou');
   };
   scrollToTop = () => {
@@ -18,7 +14,6 @@ class Upsell21 extends React.PureComponent {
     });
   };
   render() {
-    const { orderId } = this.props.url.query;
     return (
       <React.Fragment>
         <PromoSession pageType="upsellPage2" />
@@ -37,6 +32,7 @@ class Upsell21 extends React.PureComponent {
           <img
             src="/static/assets/images/upsell2-mobile/up-prod4.jpg"
             className="up-prod"
+            alt=""
           />
           <div className="clearall" />
           <div className="price-box">
@@ -44,14 +40,14 @@ class Upsell21 extends React.PureComponent {
             <p className="price-box-txt2">Save 30% Today</p>
             <p className="price-box-txt3">
               <img
-                src="images/arrow-left.png"
+                src="../../../../static/assets/images/upsell2-mobile/arrow-left.png"
                 width="77"
                 height="33"
                 alt=""
                 className="arrow-left"
               />$97.00{' '}
               <img
-                src="images/arrow-right.png"
+                src="../../../../static/assets/images/upsell2-mobile/arrow-right.png"
                 width="77"
                 height="33"
                 alt=""
@@ -80,7 +76,7 @@ class Upsell21 extends React.PureComponent {
                     alt=""
                     className="cut-icon"
                   />{' '}
-                  No, I don't want better results.
+                  No, I don&apos;t want better results.
                 </a>
               </Link>
             </p>
@@ -93,7 +89,10 @@ class Upsell21 extends React.PureComponent {
           <div className="clearall" />
 
           <div className="up-bottom-box">
-            <img src="/static/assets/images/upsell2-mobile/up1-img1.png" />
+            <img
+              src="/static/assets/images/upsell2-mobile/up1-img1.png"
+              alt=""
+            />
             <p className="box-txt1">FULL SPECTRUM FORMULA</p>
             <p className="box-txt2">
               CBD Warming Balm is INFUSED with highly a concentrated CBD which
@@ -103,7 +102,10 @@ class Upsell21 extends React.PureComponent {
           </div>
 
           <div className="up-bottom-box">
-            <img src="/static/assets/images/upsell2-mobile/up2-img2.png" />
+            <img
+              src="/static/assets/images/upsell2-mobile/up2-img2.png"
+              alt=""
+            />
             <p className="box-txt1">OFFERS ANTIOXIDANT SUPPORT</p>
             <p className="box-txt2">
               CBD Warming Balm works at a cellular level to combat free radical
@@ -113,11 +115,14 @@ class Upsell21 extends React.PureComponent {
           </div>
 
           <div className="up-bottom-box">
-            <img src="/static/assets/images/upsell2-mobile/up2-img3.png" />
+            <img
+              src="/static/assets/images/upsell2-mobile/up2-img3.png"
+              alt=""
+            />
             <p className="box-txt1">INSTANTLY RELIEVES CHRONIC PAIN</p>
             <p className="box-txt2">
               Melt away muscle fatigue, pain, swelling, and discomfort with
-              American Science CBD WARMING BALM'S deep, penetrating warmth
+              American Science CBD WARMING BALM&apos;S deep, penetrating warmth
               providing FAST relief that will amaze you.
             </p>
           </div>
@@ -142,7 +147,7 @@ class Upsell21 extends React.PureComponent {
                     alt=""
                     className="cut-icon"
                   />{' '}
-                  No, I don't want better results.
+                  No, I don&apos;t want better results.
                 </a>
               </Link>
             </p>
