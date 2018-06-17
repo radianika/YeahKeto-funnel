e2431bb10ef416f84f51ec0cc00eb6e474b8b97b
Goldinitp <goldinitp@gmail.com>
2018-06-17 14:41:35 +0530

    added hotjar snippet to head

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,19 @@ export default class MyDocument extends Document {
             src="/static/assets/js/googleTagLoader.js"
           />
           <script type="text/javascript" src="/static/assets/js/segment.js" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:890464,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+            }}
+          />
         </Head>
         <body>
           <noscript
