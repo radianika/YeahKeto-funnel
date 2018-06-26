1a3eef60d433b53b6de337de376d9ad381737293
Goldinitp <goldinitp@gmail.com>
2018-06-26 14:43:08 +0530

    adding missing treatment

--- helpers/abtasty/campaign_306753.js
+++ helpers/abtasty/campaign_306753.js
@@ -1,16 +1,16 @@
 const campaign_306753 = {
   control: {
     promo: {
-      section1Img: '/static/promo/desktop/v2/section1.jpg',
+      section1Img: '/static/promo/desktop/images/section1.jpg',
     },
     upsell1: {},
     upsell2: {},
     upsell11: {},
     upsell21: {},
   },
-  404691: {
+  404689: {
     promo: {
-      section1Img: '/static/promo/desktop/v2/section1.jpg',
+      section1Img: '/static/promo/desktop/images/section1.jpg',
     },
     upsell1: {},
     upsell2: {},
@@ -26,6 +26,15 @@ const campaign_306753 = {
     upsell11: {},
     upsell21: {},
   },
+  404691: {
+    promo: {
+      section1Img: '/static/promo/desktop/v2/section1.jpg',
+    },
+    upsell1: {},
+    upsell2: {},
+    upsell11: {},
+    upsell21: {},
+  }
 };
 
 export {
