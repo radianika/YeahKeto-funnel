08f6591ea8c2150209f184e15488760b8ca88a63
Chetan Bhuwania <chetan@numerin.io>
2018-06-27 19:34:58 +0530

    fixes casing

--- react/components/promo/mobile/PromoSectionFourMobile.js
+++ react/components/promo/mobile/PromoSectionFourMobile.js
@@ -24,7 +24,7 @@ class PromoSectionFourMobile extends React.PureComponent {
         </ul>
         <p className="clearall" />
         <p className="s2-txt1">
-          <strong>American Science CBD oil</strong> positively modulates the ECS
+          <strong>American Science CBD Oil</strong> positively modulates the ECS
           system, improving mental clarity, sleep cycles, healthy inflammatory
           response, cognitive function & more.{' '}
         </p>
@@ -33,21 +33,21 @@ class PromoSectionFourMobile extends React.PureComponent {
           <li>
             <i className="s4-icons sprite3 sprite-s4-icon4" />{' '}
             <span>Boosts Cognitive Function </span>
-            <br />CBD oil supports optimal brain function, improving focus,
+            <br />CBD Oil supports optimal brain function, improving focus,
             mental clarity, and memory recall. It also helps slow down the
             age-related decline in cognitive health.{' '}
           </li>
           <li>
             <i className="s4-icons sprite3 sprite-s4-icon5" />{' '}
             <span>Supports Joint Health </span>
-            <br />CBD oil lubricates the joints to support improved flexibility
+            <br />CBD Oil lubricates the joints to support improved flexibility
             and mobility. It also helps deliver essential cannabinoids to treat
             chronic aches and pains.{' '}
           </li>
           <li>
             <i className="s4-icons sprite3 sprite-s4-icon6" />{' '}
             <span>Reduces Anxiety & Stress </span>
-            <br />CBD oil has a positive impact on mood patterns and sleep
+            <br />CBD Oil has a positive impact on mood patterns and sleep
             cycles. This helps promote a feeling of calm and relaxation to
             combat stress and anxiety.
           </li>
