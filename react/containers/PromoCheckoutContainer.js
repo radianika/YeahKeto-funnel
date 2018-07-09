86d800d8082f8700ca20d4fe1b8ea79a961b98f5
Goldinitp <goldinitp@gmail.com>
2018-07-09 20:22:35 +0530

    call order change

--- react/containers/PromoCheckoutContainer.js
+++ react/containers/PromoCheckoutContainer.js
@@ -9,10 +9,10 @@ import { PromoCheckoutPaymentForm } from 'react/components/promo/desktop';
 import { packages } from 'helpers';
 
 const packMapping = {
-  210 : '5-bottle-order-1',
-  209 : '3-bottle-order-1',
-  208 : '1-bottle-order-1'
-}
+  210: '5-bottle-order-1',
+  209: '3-bottle-order-1',
+  208: '1-bottle-order-1',
+};
 
 class PromoCheckout extends React.PureComponent {
   constructor() {
@@ -28,6 +28,7 @@ class PromoCheckout extends React.PureComponent {
 
   submitBillingForm = values => {
     this.sendTransactionDetails();
+    this.sendTransactionDetailsPackInfo();
     this.props.placeOrder({
       values,
       pack: this.state.selected,
@@ -60,7 +61,9 @@ class PromoCheckout extends React.PureComponent {
   sendTransactionDetailsPackInfo = () => {
     const id = this.state.selected.id;
     const revenue = this.state.selected.packagePrice;
-    const abtastyParams = JSON.parse(localStorage.getItem('abtastyParams_313018'));
+    const abtastyParams = JSON.parse(
+      localStorage.getItem('abtastyParams_313018'),
+    );
     const body = {
       name: packMapping[id],
       id,
@@ -121,7 +124,6 @@ class PromoCheckout extends React.PureComponent {
                       }
                       onClick={() => {
                         this.setState({ selected: pack });
-                        this.sendTransactionDetailsPackInfo();
                       }}
                     >
                       <div className="pkg-hdbox">
