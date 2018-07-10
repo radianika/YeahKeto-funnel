cfa52074a6d309ab191154166cddfe7078abe777
Sahil Sharma <sahilaug@gmail.com>
2018-07-10 17:08:53 +0530

    remove abtasty calls for test8

--- server/index.js
+++ server/index.js
@@ -347,13 +347,13 @@ app.prepare().then(() => {
   server.get('/promo/mobile/shipping', async (req, res) => {
     try {
       const sessionId = await getSessionId(req, res);
-      const { visitorId, isNew } = await getVisitorId(req, res);
-      const variationId = await getVariationForVisitor(visitorId, '312844');
+      // const { visitorId } = await getVisitorId(req, res);
+      // const variationId = await getVariationForVisitor(visitorId, '312844');
 
       return app.render(req, res, '/promo-mobile-shipping', {
         sessionId,
-        visitorId,
-        variationId,
+        // visitorId,
+        // variationId,
       });
     } catch (error) {
       Raven.captureException(error);
