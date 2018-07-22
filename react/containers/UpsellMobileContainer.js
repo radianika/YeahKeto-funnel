9ef094298f9e10aac99bf7ab16291649452c5458
Vikas Neha Ojha <neha.nehavikas.vikas@gmail.com>
2018-07-22 06:07:57 +0530

    Added FB Pixel

--- react/containers/UpsellMobileContainer.js
+++ react/containers/UpsellMobileContainer.js
@@ -100,6 +100,22 @@ class UpsellMobileContainerComponent extends React.PureComponent {
       this.props.abtastyParams.prev.indexOf('upsell11') > -1;
     return (
       <div id="container">
+
+        <script>{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '287233031840657');
+          fbq('track', 'PageView');
+        `}</script>
+        <noscript><img height="1" width="1" style={{display: 'none'}}
+                       src="https://www.facebook.com/tr?id=287233031840657&ev=PageView&noscript=1"/></noscript>
+
         {upsell === 1 &&
           offerId && (
             <iframe
