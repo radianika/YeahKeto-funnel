e7f7571c2e95d378dbc65eb8fe4a768c54e7f0b4
Sahil Sharma <sahilaug@gmail.com>
2018-07-24 18:51:06 +0530

    wire abtasty api calls

--- react/components/promo/FooterPromo.js
+++ react/components/promo/FooterPromo.js
@@ -104,7 +104,7 @@ class FooterPromoComponent extends React.PureComponent {
         'mobile-hp-last-module-badge-test-rush-my-order',
         'mobile-hp-last-module-picture-test-rush-my-order',
         'mobile-hp-first-module-badge-test-rush-my-order',
-        'mobile-hp-top-module-symbol1-test-rush-my-order',
+        'mobile-hp-as-advertised-on-text-test-rush-my-order',
       ];
       const tracking_data = {
         visitor_id: abtastyParams.visitorId,
