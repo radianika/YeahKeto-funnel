1d1ef715ee3aa8f438d1dd1a129f083f783a070e
vladimir <vladimir@starlightgroup.io>
2018-07-31 13:11:20 +0300

    Add missing event

--- pages/promo-mobile-confirm.js
+++ pages/promo-mobile-confirm.js
@@ -41,7 +41,7 @@ class Confirm extends React.PureComponent {
   }
 
   postCampaignActivatedEvent() {
-    const campaigns = ['317687', '318677'];
+    const campaigns = ['317687', '318677', '319527'];
     const tracking_data = {
       device_type: 'MOBILE_PHONE',
       ip: this.props.abtastyParams.ip,
