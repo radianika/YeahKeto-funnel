99f433ad2cabadbc166a08d605339ca868299b92
Goldinitp <goldinitp@gmail.com>
2019-02-09 14:22:56 +0530

    files updated

--- react/components/upsell/desktop/Upsell11.js
+++ react/components/upsell/desktop/Upsell11.js
@@ -26,46 +26,112 @@ class Upsell11Component extends React.PureComponent {
         <div className="up-bg">
           <div className="inner-container">
             <div className="up-inr">
-              <img src="/static/promo/desktop/images/images/logo.png" className="up-logo" />
-              <img src="/static/promo/desktop/images/images/chk-hdr.png" alt className="up-steps" />
+              <img
+                src="/static/promo/desktop/img/logo.png"
+                className="up-logo"
+              />
+              <img
+                src="/static/promo/desktop/img/chk-hdr.png"
+                alt
+                className="up-steps"
+              />
               <div className="inr-chk inr-pack inr-upsell">
                 <div className="upsell-sec">
-                  <p className="up-hdg">WAIT! YOU QUALIFY FOR A LIMITED TIME DISCOUNT</p>
-                  <p className="up-sub-hdg">68% of customers who purchase <span>Yeah Keto</span> also purchase <span>Yeah Forskolin</span></p>
+                  <p className="up-hdg">
+                    WAIT! YOU QUALIFY FOR A LIMITED TIME DISCOUNT
+                  </p>
+                  <p className="up-sub-hdg">
+                    68% of customers who purchase <span>Yeah Keto</span> also
+                    purchase <span>Yeah Forskolin</span>
+                  </p>
                   <div className="up-box-bg up2-box-bg">
                     <div className="up-lft up2-lft up1-1-lft">
-                      <img src="/static/promo/desktop/images/images/up1-1-prd.png" alt className="up-prd" />
-                      <img src="/static/promo/desktop/images/images/up2-lft-img1.png" alt className="up-lft-img1" />
+                      <img
+                        src="/static/promo/desktop/img/up1-1-prd.png"
+                        alt
+                        className="up-prd"
+                      />
+                      <img
+                        src="/static/promo/desktop/img/up2-lft-img1.png"
+                        alt
+                        className="up-lft-img1"
+                      />
                       <div className="up-seal">
-                        <p>save<br />$44%</p>
+                        <p>
+                          save<br />$44%
+                        </p>
                       </div>
                     </div>
                     <div className="up-rgt">
                       <p className="up-rgt-txt1">Boost Your Metabolism With</p>
-                      <img src="/static/promo/desktop/images/images/up2-logo.png" className="up1-logo" />
-                      <p className="up-rgt-txt3 up2-rgt-txt3">Advanced Metabolic Support Formula*</p>
+                      <img
+                        src="/static/promo/desktop/img/up2-logo.png"
+                        className="up1-logo"
+                      />
+                      <p className="up-rgt-txt3 up2-rgt-txt3">
+                        Advanced Metabolic Support Formula*
+                      </p>
                       <ul className="up-list">
                         <li>
-                          <img src="/static/promo/desktop/images/images/up2-tick.png" alt className="for-desk" />
-                          <p>Break Down<br /><span>Fatty Tissues</span></p>
+                          <img
+                            src="/static/promo/desktop/img/up2-tick.png"
+                            alt
+                            className="for-desk"
+                          />
+                          <p>
+                            Break Down<br />
+                            <span>Fatty Tissues</span>
+                          </p>
                         </li>
                         <li>
-                          <img src="/static/promo/desktop/images/images/up2-tick.png" alt className="for-desk" />
-                          <p>Preserve<br /><span>Lean Muscle</span></p>
+                          <img
+                            src="/static/promo/desktop/img/up2-tick.png"
+                            alt
+                            className="for-desk"
+                          />
+                          <p>
+                            Preserve<br />
+                            <span>Lean Muscle</span>
+                          </p>
                         </li>
                         <li>
-                          <img src="/static/promo/desktop/images/images/up2-tick.png" alt className="for-desk" />
-                          <p>Boost<br /><span>Metabolism</span></p>
+                          <img
+                            src="/static/promo/desktop/img/up2-tick.png"
+                            alt
+                            className="for-desk"
+                          />
+                          <p>
+                            Boost<br />
+                            <span>Metabolism</span>
+                          </p>
                         </li>
                       </ul>
                       <div className="up-prd-info">
-                        <p className="up-prd-p1 up2-prd-p1"><span>Buy 2 Bottles + Get 1 Free</span><br />Save 60% Today </p>
-                        <p className="prd-prc prd1-1-prc">
-                          <span><img src="/static/promo/desktop/images/images/price-cut.png" alt />$120/<sup>ea</sup></span> $77/<sup>ea</sup>
+                        <p className="up-prd-p1 up2-prd-p1">
+                          <span>Buy 2 Bottles + Get 1 Free</span>
+                          <br />Save 60% Today{' '}
                         </p>
-                        <a onClick={this.upgrade}><img src="/static/promo/desktop/images/images/up-btn.png" alt className="up-btn" /></a>
+                        <p className="prd-prc prd1-1-prc">
+                          <span>
+                            <img
+                              src="/static/promo/desktop/img/price-cut.png"
+                              alt
+                            />$120/<sup>ea</sup>
+                          </span>{' '}
+                          $77/<sup>ea</sup>
+                        </p>
+                        <a onClick={this.upgrade}>
+                          <img
+                            src="/static/promo/desktop/img/up-btn.png"
+                            alt
+                            className="up-btn"
+                          />
+                        </a>
                         <a onClick={this.skipUpsell}>
-                          <p className="no-p"> No thanks, I’m not interested </p>
+                          <p className="no-p">
+                            {' '}
+                            No thanks, I’m not interested{' '}
+                          </p>
                         </a>
                       </div>
                     </div>
